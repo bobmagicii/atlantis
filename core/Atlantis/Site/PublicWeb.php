ef31dda89e0eef245e9d160b9b96275395966ffd
bobmagicii <bob@majdak.net>
2017-02-11 14:46:32 -0600

    add error storage and cheap error storage

--- core/Atlantis/Site/PublicWeb.php
+++ core/Atlantis/Site/PublicWeb.php
@@ -14,7 +14,8 @@ class PublicWeb {
 	$Post    = NULL,
 	$Router  = NULL,
 	$Surface = NULL,
-	$User    = NULL;
+	$User    = NULL,
+	$Errors  = NULL;
 
 	////////////////////////////////////////////////////////////////
 	////////////////////////////////////////////////////////////////
@@ -27,8 +28,33 @@ class PublicWeb {
 		$this->Router = Nether\Stash::Get('Router');
 		$this->Surface = new Nether\Surface;
 		$this->User = Nether\Stash::Get('User');
+		$this->Errors = new Nether\Object\Datastore;
+
+		Nether\Ki::Queue(
+			'surface-render-scope',
+			function(Array &$Scope):
+			Void { $Scope['route'] = $this; return; },
+			TRUE
+		);
 
 		return;
+	}
+
+	////////////////////////////////////////////////////////////////
+	////////////////////////////////////////////////////////////////
+
+	public function
+	Redirect(String $URL, Int $Code=303):
+	Void {
+	/*//
+	@todo actually use the specific code.
+	@date 2017-02-11
+	//*/
+
+		header("HTTP/1.1 303 See Other");
+		header("Location: {$URL}");
+
+		exit(0);
 	}
 
 }
