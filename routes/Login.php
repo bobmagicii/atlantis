cb5c22b050ac0241382a6b3b9b8b6a17c69bdc24
bobmagicii <bob@majdak.net>
2017-02-11 14:47:38 -0600

    working login page

--- routes/Login.php
+++ routes/Login.php
@@ -3,6 +3,9 @@
 namespace Routes;
 use \Routes   as Routes;
 use \Atlantis as Atlantis;
+use \Nether   as Nether;
+
+use \Atlantis\Site\Error\Inline as Error;
 
 class Login
 extends Atlantis\Site\PublicWeb {
@@ -11,10 +14,8 @@ extends Atlantis\Site\PublicWeb {
 	Index():
 	Void {
 
-		if($this->Post->Account && $this->Post->Password) {
-			$this->HandleLogin();
-			return;
-		}
+		if($this->Post->Account && $this->Post->Password)
+		$this->HandleLogin();
 
 		$this->Surface->Area('login/index');
 		return;
@@ -24,6 +25,26 @@ extends Atlantis\Site\PublicWeb {
 	HandleLogin():
 	Void {
 
+		$User = Nether\Auth\User::Get($this->Post->Account);
+
+		if(!$User) {
+			$this->Errors->Push(new Error([
+				'Error'   => 1,
+				'Message' => 'Invalid account or password.'
+			]));
+			return;
+		}
+
+		if(!$User->IsValidPassword($this->Post->Password)) {
+			$this->Errors->Push(new Error([
+				'Error'   => 2,
+				'Message' => 'Invalid account or password.'
+			]));
+			return;
+		}
+
+		Nether\Auth\User::LaunchSession($User);
+		$this->Redirect('/');
 		return;
 	}
 
