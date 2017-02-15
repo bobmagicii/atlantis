e567462cafccf70c1d7252b221d8c108903f0ae9
bobmagicii <bob@majdak.net>
2017-02-14 19:45:15 -0600

    config clean up

--- conf/atlantis.conf.php
+++ conf/atlantis.conf.php
@@ -1,26 +1,28 @@
 <?php
 
-////////////////////////////////////////////////////////////////////////////////
-// Application Configuration ///////////////////////////////////////////////////
-
 Nether\Option::Set([
-	// application settings.
-	'app-name' => 'Atlantis',
-	'app-short-desc' => 'Lulz',
-	'app-long-desc' => 'Lulz',
 
-	// framework settings.
+	////////////////////////////////////////////////////////////////
+	// Application Config //////////////////////////////////////////
+
+	'app-name'       => 'Atlantis',
+	'app-short-desc' => 'Lulz',
+	'app-long-desc'  => 'Lulz',
+
 	'nether-web-root' => sprintf('%s/www',ProjectRoot),
 	'nether-web-path' => '/',
 
-	// cache config.
-	// delete cache-drivers-load for production.
-	'cache-stash-name' => 'Cache',
-	'cache-key-prefix' => 'atlantis-',
+	////////////////////////////////////////////////////////////////
+	// Library Config //////////////////////////////////////////////
+
+	// you wont want to mess with these much i promise.
+
+	'surface-auto-stash' => 'Surface',
+
+	'cache-stash-name'   => 'Cache',
+	'cache-key-prefix'   => 'atlantis-',
 	'cache-drivers-load' => [
 		'App'  => 'Nether\\Cache\\Appcache',
-	],
+	]
 
-	// surface config.
-	'surface-auto-stash' => 'Surface'
 ]);
