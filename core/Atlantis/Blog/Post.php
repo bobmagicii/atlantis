e2450e1089a5a02e4997f672c5f1b6328af91bd3
bobmagicii <bob@majdak.net>
2017-02-20 17:14:47 -0600

    working on blog post class

--- core/Atlantis/Blog/Post.php
+++ core/Atlantis/Blog/Post.php
@@ -4,6 +4,8 @@ namespace Atlantis\Blog;
 use \Atlantis as Atlantis;
 use \Nether   as Nether;
 
+use \Exception as Exception;
+
 class Post
 extends Nether\Object {
 
@@ -11,6 +13,7 @@ extends Nether\Object {
 	$PropertyMap = [
 		'post_id'      => 'ID:int',
 		'user_id'      => 'OwnerID:int',
+		'blog_id'      => 'BlogID:int',
 		'post_ptime'   => 'TimePosted:int',
 		'post_utime'   => 'TimeUpdated:int',
 		'post_draft'   => 'Draft:bool',
@@ -35,13 +38,21 @@ extends Nether\Object {
 	////////////////
 
 	protected
-	$OwnerID = 0;
+	$OwnerID = 0,
+	$Owner   = NULL;
 
 	public function
 	GetOwnerID():
 	Int {
 
 		return $this->OwnerID;
+	}
+
+	public function
+	GetOwner():
+	Nether\Auth\User {
+
+
 	}
 
 	////////////////
@@ -57,7 +68,7 @@ extends Nether\Object {
 		return $this->TimePosted;
 	}
 
-	public functoin
+	public function
 	GetDatePosted():
 	String {
 
@@ -125,6 +136,101 @@ extends Nether\Object {
 
 	////////////////////////////////////////////////////////////////
 	////////////////////////////////////////////////////////////////
+
+	public function
+	__ready():
+	Void {
+
+		$this->Owner = Nether\Auth\User::GetByID($this->OwnerID);
+		return;
+	}
+
+	////////////////////////////////////////////////////////////////
+	////////////////////////////////////////////////////////////////
+
+	static public function
+	GetByID(Int $ID):
+	?self {
+
+		$Result = Nether\Database::Get()
+		->NewVerse()
+		->Select('Posts')
+		->Fields('*')
+		->Where('post_id=:PostID')
+		->Limit(1)
+		->Query([
+			':PostID' => $ID
+		]);
+
+		if(!$Result->IsOK())
+		throw new Exception('Post::GetByID critikal phail');
+
+		if(!$Result->GetCount())
+		return NULL;
+
+		return new self($Result->Next());
+	}
+
+	////////////////////////////////////////////////////////////////
+	////////////////////////////////////////////////////////////////
+
+	static public function
+	Create($Opt=NULL):
+	self {
+
+		$Result = NULL;
+		$ID = NULL;
+
+		////////
+
+		$Opt = new Nether\Object($Opt,[
+			// required.
+			'OwnerID'     => NULL,
+			'Title'       => NULL,
+			'Content'     => NULL,
+
+			// meta.
+			'TimePosted'  => time(),
+			'TimeUpdated' => time(),
+			'Draft'       => FALSE
+		]);
+
+		////////
+
+		if($Opt->OwnerID === NULL)
+		throw new Exception('OwnerID cannot be empty.');
+
+		if(!$Opt->Title === NULL)
+		throw new Exception('Title cannot be empty.');
+
+		if(!$Opt->Content === NULL)
+		throw new Exception('Content cannot be empty it a blog ffs');
+
+		////////
+
+		$Result = Nether\Database::Get()
+		->NewVerse()
+		->Insert('Posts')
+		->Values([
+			'user_id'      => ':OwnerID',
+			'post_ptime'   => ':TimePosted',
+			'post_utime'   => ':TimeUpdated',
+			'post_draft'   => ':Draft',
+			'post_title'   => ':Title',
+			'post_content' => ':Content'
+		])
+		->Query($Opt);
+
+		if(!$Result->IsOK())
+		throw new Exception('Post::Create critikal phail');
+
+		if(!($ID = $Result->GetInsertID()))
+		throw new Exception('Post::Create confusing phail');
+
+		////////
+
+		return self::GetByID((Int)$ID);
+	}
 
 }
 
