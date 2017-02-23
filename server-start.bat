cd92b55de3131a94efd9b090f0113d11f56ccb1f
bobmagicii <bob@majdak.net>
2017-02-23 01:01:49 -0600

    update server-start for moar

--- server-start.bat
+++ server-start.bat
@@ -2,6 +2,11 @@
 
 rem start the mysql server.
 start "Atlantis Dev MYSQL" /B mysqld --log_syslog=0
+timeout 2 /nobreak
 
 rem start the php server.
 start "Atlantis Dev Web" /B php -S localhost:80 -t www > NUL 2> NUL
+timeout 2 /nobreak
+
+rem open the browser
+start http://localhost
