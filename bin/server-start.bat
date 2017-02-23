a00f86fcf6e83b941ece41aadca3161e6eb42e02
bobmagicii <bob@majdak.net>
2017-02-23 17:09:07 -0600

    move scripts to bin dir. add .\bin to ur path for win.

--- bin/server-start.bat
+++ bin/server-start.bat
@@ -1,10 +1,12 @@
 @echo off
 
 rem start the mysql server.
+echo Starting MySQL...
 start "Atlantis Dev MYSQL" /B mysqld --log_syslog=0
 timeout 2 /nobreak
 
 rem start the php server.
+echo Starting PHP Web Server...
 start "Atlantis Dev Web" /B php -S localhost:80 -t www > NUL 2> NUL
 timeout 2 /nobreak
 
