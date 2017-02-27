98047585dd5d2dc9f2804bd6183a4290bb360dca
bobmagicii <bob@majdak.net>
2017-02-27 13:23:23 -0600

    more fleshing out of the post class

--- core/Atlantis/Blog/Post.php
+++ core/Atlantis/Blog/Post.php
@@ -30,8 +30,41 @@ extends Nether\Object {
 	public function
 	GetID():
 	Int {
+	/*//
+	return the unique id for this object.
+	//*/
 
 		return $this->ID;
+	}
+
+	////////////////
+	////////////////
+
+	protected
+	$BlogID = 0,
+	$Blog   = NULL;
+
+	public function
+	GetBlogID():
+	Int {
+	/*//
+	return the id of the blog this post belongs to.
+	//*/
+
+		return $this->BlogID;
+	}
+
+	public function
+	GetBlogObject():
+	Atlantis\Blog {
+	/*//
+	return the blog object this post belongs to.
+	//*/
+
+		if(!$this->Blog)
+		$this->Blog = Atlantis\Blog::GetByID((Int)$this->BlogID);
+
+		return $this->Blog;
 	}
 
 	////////////////
@@ -44,15 +77,24 @@ extends Nether\Object {
 	public function
 	GetUserID():
 	Int {
+	/*//
+	return the id of the user that created this post.
+	//*/
 
 		return $this->UserID;
 	}
 
 	public function
-	GetUser():
-	?Atlantis\User {
+	GetUserObject():
+	Atlantis\User {
+	/*//
+	return the user object that created this post.
+	//*/
 
-		return Atlantis\User::Get((Int)$this->UserID);
+		if(!$this->User)
+		$this->User = Atlantis\User::GetByID((Int)$this->UserID);
+
+		return $this->User;
 	}
 
 	////////////////
@@ -64,6 +106,9 @@ extends Nether\Object {
 	public function
 	GetTimePosted():
 	Int {
+	/*//
+	return the time in seconds that describes when this post was created.
+	//*/
 
 		return $this->TimePosted;
 	}
@@ -71,8 +116,25 @@ extends Nether\Object {
 	public function
 	GetDatePosted():
 	String {
+	/*//
+	return the formatted date that describes when this post was created.
+	//*/
 
 		return date('Y-m-d',$this->TimePosted);
+	}
+
+	public function
+	GetDateTimePosted($TwentyFour=TRUE):
+	String {
+	/*//
+	return the formatted date and time that describes when this post was
+	created. returns 24hr format by default.
+	//*/
+
+		return date(
+			(($TwentyFour)?('Y-m-d H:i'):('Y-m-d g:ia')),
+			$this->TimePosted
+		);
 	}
 
 	////////////////
@@ -84,6 +146,9 @@ extends Nether\Object {
 	public function
 	GetTimeUpdated():
 	Int {
+	/*//
+	return the time in seconds that describes when this post was updated.
+	//*/
 
 		return $this->TimeUpdated;
 	}
@@ -91,8 +156,25 @@ extends Nether\Object {
 	public function
 	GetDateUpdated():
 	String {
+	/*//
+	return the formatted date that describes when this post was updated.
+	//*/
 
 		return date('Y-m-d',$this->TimeUpdated);
+	}
+
+	public function
+	GetDateTimeUpdated($TwentyFour=TRUE):
+	String {
+	/*//
+	return the formatted date and time that describes when this post was
+	created. returns 24hr format by default.
+	//*/
+
+		return date(
+			(($TwentyFour)?('Y-m-d H:i'):('Y-m-d g:ia')),
+			$this->TimeUpdated
+		);
 	}
 
 	////////////////
@@ -104,6 +186,9 @@ extends Nether\Object {
 	public function
 	IsDraft():
 	Bool {
+	/*//
+	return if this post is flagged as a draft or not.
+	//*/
 
 		return $this->Draft;
 	}
@@ -117,6 +202,9 @@ extends Nether\Object {
 	public function
 	GetTitle():
 	String {
+	/*//
+	return the title that was given to this post.
+	//*/
 
 		return $this->Title;
 	}
@@ -130,6 +218,9 @@ extends Nether\Object {
 	public function
 	GetContent():
 	String {
+	/*//
+	return the text content of this post.
+	//*/
 
 		return $this->Content;
 	}
@@ -140,8 +231,11 @@ extends Nether\Object {
 	public function
 	__ready():
 	Void {
+	/*//
+	prepare some data for this object.
+	//*/
 
-		$this->User = Atlantis\User::GetByID($this->UserID);
+		$this->Draft = (Bool)$this->Draft;
 
 		return;
 	}
@@ -150,11 +244,29 @@ extends Nether\Object {
 	////////////////////////////////////////////////////////////////
 
 	static public function
-	ListByBlogID(Int $BlogID):
+	ListByBlogID(Int $BlogID, $Opt=NULL):
 	Array {
+	/*//
+	fetch a list of all the posts that belong to a specified blog.
+	//*/
 
 		$Output = [];
 		$Row = NULL;
+
+		////////
+
+		$Opt = Nether\Object::Get($Opt,[
+			// options
+			'Page'   => 1,
+			'Limit'  => 10,
+
+			// calculations
+			'Offset' => NULL
+		]);
+
+		$Opt->Page = Atlantis\Util\Filters::PageNumber($Opt->Page);
+		$Opt->Limit = Atlantis\Util\Filters::NumberLimit25($Opt->Limit);
+		$Opt->Offset = ($Opt->Page - 1) * $Opt->Limit;
 
 		////////
 
@@ -163,6 +275,8 @@ extends Nether\Object {
 		->Select('BlogPosts')
 		->Fields('*')
 		->Where('blog_id=:BlogID')
+		->Offset($Opt->Offset)
+		->Limit($Opt->Limit)
 		->Query([
 			':BlogID' => $BlogID
 		]);
@@ -181,6 +295,9 @@ extends Nether\Object {
 	static public function
 	GetByID(Int $ID):
 	?self {
+	/*//
+	get a specific blog post by id.
+	//*/
 
 		$Result = Nether\Database::Get()
 		->NewVerse()
@@ -207,6 +324,9 @@ extends Nether\Object {
 	static public function
 	Create($Opt=NULL):
 	self {
+	/*//
+	create a new post with the specified data.
+	//*/
 
 		$Result = NULL;
 		$ID = NULL;
