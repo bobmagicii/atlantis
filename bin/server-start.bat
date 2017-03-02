1410d9c2017c3387347332afbd6cfdc7dddc9e55
bobmagicii <bob@majdak.net>
2017-03-02 13:32:00 -0600

    bump dev server delay

--- bin/server-start.bat
+++ bin/server-start.bat
@@ -3,12 +3,12 @@
 rem start the mysql server.
 echo Starting MySQL...
 start "Atlantis Dev MYSQL" /B mysqld --log_syslog=0
-timeout 2 /nobreak
+timeout 3 /nobreak
 
 rem start the php server.
 echo Starting PHP Web Server...
 start "Atlantis Dev Web" /B php -S localhost:80 -t www > NUL 2> NUL
-timeout 2 /nobreak
+timeout 3 /nobreak
 
 rem open the browser
 start http://localhost
