1e48af806d3cdd45014082776b0ba7b04d8c2df6
bobmagicii <bob@majdak.net>
2017-03-02 19:12:35 -0600

    add object property trait to item

---
+++ core/Atlantis/Properties/Object.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Atlantis\Properties;
+
+trait Object {
+/*//
+provide a id property to any object that requests to have one with get, set,
+and an actionable method.
+//*/
+
+	protected
+	$Object = NULL;
+
+	////////
+	////////
+
+	public function
+	GetObject() {
+	/*//
+	@return Object
+	//*/
+
+		return $this->Object;
+	}
+
+	public function
+	HasObject():
+	Bool {
+
+		return is_object($this->Object);
+	}
+
+	public function
+	SetObject($Object):
+	self {
+	/*//
+	@argv Object $Object
+	//*/
+
+		if(is_object($Object))
+		$this->Object = $Object;
+
+		return $this;
+	}
+
+	////////
+	////////
+
+	public function
+	Object($Object=NULL) {
+
+		if($Object === NULL)
+		return $this->GetObject();
+
+		else
+		return $this->SetObject($Object);
+	}
+
+}
