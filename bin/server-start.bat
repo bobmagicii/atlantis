0847e364eb908d8830fbf2e8ba19caca3b793a53
bobmagicii <bob@majdak.net>
2020-01-21 15:54:46 -0600

    update boot script for my dev env

--- bin/server-start.bat
+++ bin/server-start.bat
@@ -2,12 +2,12 @@
 
 rem start the mysql server.
 echo Starting MySQL...
-start "Atlantis Dev MYSQL" /B mysqld --log_syslog=0
+start "Atlantis Dev MYSQL" /B mysqld57 --log_syslog=0
 timeout 3 /nobreak
 
 rem start the php server.
 echo Starting PHP Web Server...
-start "Atlantis Dev Web" /B php -S localhost:80 -t www > NUL 2> NUL
+start "Atlantis Dev Web" /B php74 -S localhost:80 -t www > NUL 2> NUL
 timeout 3 /nobreak
 
 rem open the browser
