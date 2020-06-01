03f28beb3f45647554f7a5404dddff9d5c756dec
bobmagicii <bob@majdak.net>
2020-06-01 07:19:37 +0000

    todo note

--- routes/Api/V1/Blog.php
+++ routes/Api/V1/Blog.php
@@ -24,8 +24,13 @@ extends Atlantis\Site\ProtectedAPI {
 		elseif($this->Get->Alias)
 		$Blog = Atlantis\Prototype\Blog::GetByAlias($this->Get->Alias);
 
-		if(!$Blog)
-		$this->Quit(1,'not found');
+		if(!$Blog) {
+			// todo: write a blog post about how 404 makes no god damn
+			// sense since you found and succesfully queried the api
+			// and that its none of the transport protocol's god damn
+			// business to need to know if the result set was empty.
+			$this->Quit(1,'not found');
+		}
 
 		$this
 		->SetPayload($Blog);
