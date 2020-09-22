91cbbc3dd96d921fafd0fb0bb90ade3d59aeec4b
bobmagicii <bob@pegasusgate.net>
2020-09-22 06:36:31 +0000

    remove unused code, shard scale code

--- routes/Media.php
+++ routes/Media.php
@@ -23,72 +23,19 @@ extends Atlantis\Site\PublicWeb {
 	as much as that would make this code a lot cleaner.
 	//*/
 
-		$Type = NULL;
-		$MaxAge = 86400;
-		$ModifiedTime = NULL;
-		$Expires = NULL;
-		$Modified = NULL;
-		$LastModified = NULL;
-		$IfModifiedSince = NULL;
-
-		////////
-
 		($this->Surface)
 		->SetAutoRender(FALSE)
 		->Stop();
 
-		$Original = sprintf(
-			'%s/data/usr/img/%s/o.%s',
-			ProjectRoot,
-			$Path,
-			$Ext
-		);
-
-		$Filepath = sprintf(
-			'%s/data/usr/img/%s/%s.%s',
-			ProjectRoot,
-			$Path,
-			$Name,
-			$Ext
-		);
+		$ImageRoot = sprintf('%s/data/usr/img/%s',ProjectRoot,$Path);
+		$Original = sprintf('%s/o.%s',$ImageRoot,$Ext);
+		$Filepath = sprintf('%s/%s.%s',$ImageRoot,$Name,$Ext);
 
 		if(!file_exists($Filepath)) {
 			if($Name === 'o')
 			$this->Quit(404);
 
-			$Size = 69;
-
-			switch($Name) {
-				case 'th': {
-					$Size = 300;
-					break;
-				}
-				case 'sm': {
-					$Size = 500;
-					break;
-				}
-				case 'md': {
-					$Size = 800;
-					break;
-				}
-				default: {
-					$Size = 1200;
-					$Name = 'lg';
-					break;
-				}
-			}
-
-			$Image = new Imagick($Original);
-
-			if($Ext === 'jpg') {
-				$Image->SetImageFormat('jpeg');
-				$Image->SetImageCompression(Imagick::COMPRESSION_JPEG);
-				$Image->SetImageCompressionQuality(92);
-			}
-
-			$Image->ResizeImage($Size,$Size,Imagick::FILTER_CATROM,1.0,TRUE);
-			$Image->WriteImage(sprintf('%s/%s.%s',dirname($Original),$Name,$Ext));
-			$Image->Destroy();
+			$this->ScaleImage($Original,$Name,$Ext);
 		}
 
 		////////
@@ -99,19 +46,51 @@ extends Atlantis\Site\PublicWeb {
 
 		$Cache->Send();
 		return;
+	}
 
-		////////
+	protected function
+	ScaleImage(String $Original, String $Name, String $Ext):
+	Void {
+	/*//
+	@date 2020-09-22
+	//*/
 
-		$Type = mime_content_type($Filepath);
-		$Expires = gmdate('D, d M Y H:i:s T',strtotime('+1 Day'));
-		$Modified = gmdate('D, d M Y H:i:s T',$ModifiedTime);
+		$Output = sprintf('%s/%s.%s',dirname($Original),$Name,$Ext);
+		$Size = 69;
 
-		header('Pragma: cache');
-		header(sprintf('Expires: %s',$Expires));
-		header(sprintf('Last-Modified: %s',$Modified));
-		header(sprintf('Cache-Control: max-age=%s',$MaxAge));
-		header(sprintf('Content-Type: %s',$Type));
-		readfile($Filepath);
+		switch($Name) {
+			case 'th': {
+				$Size = 300;
+				break;
+			}
+			case 'sm': {
+				$Size = 500;
+				break;
+			}
+			case 'md': {
+				$Size = 800;
+				break;
+			}
+			default: {
+				$Size = 1200;
+				$Name = 'lg';
+				break;
+			}
+		}
+
+		$Image = new Imagick($Original);
+		$Image->ResizeImage($Size,$Size,Imagick::FILTER_CATROM,1.0,TRUE);
+
+		// @todo 2020-09-22 handle rotation bullshit from phone photos
+
+		if(preg_match('/^jpe?g$/i',$Ext)) {
+			$Image->SetImageFormat('jpeg');
+			$Image->SetImageCompression(Imagick::COMPRESSION_JPEG);
+			$Image->SetImageCompressionQuality(92);
+		}
+
+		$Image->WriteImage($Output);
+		$Image->Destroy();
 
 		return;
 	}
