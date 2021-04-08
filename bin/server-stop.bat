9c9b0805c76ccb4a4387218163939fc6901accb4
bobmagicii <bob@pegasusgate.net>
2021-04-08 22:40:25 +0000

    thusfar

--- bin/server-stop.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-rem gracefully ask mysql to shut down.
-echo Shutting MySQL Down...
-mysqladmin57 shutdown -u root -p
-
-rem but just kill php yolo.
-echo Shutting PHP Server Down...
-taskkill /IM php.exe /F
