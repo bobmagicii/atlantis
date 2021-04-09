2554c9ec382faae79a9a157abd39b9e95f131033
bobmagicii <bob@pegasusgate.net>
2021-04-09 03:20:10 +0000

    dont show source blog info while literally on that blog

--- core/Atlantis/Element/PostListing.php
+++ core/Atlantis/Element/PostListing.php
@@ -15,4 +15,28 @@ extends Atlantis\Element {
 	Atlantis\Properties\Subtitle,
 	Atlantis\Properties\ItemListStore;
 
+	protected bool
+	$ShowSourceBlog = TRUE;
+
+	public function
+	GetShowSourceBlog():
+	bool {
+	/*//
+	@date 2021-04-09
+	//*/
+
+		return $this->ShowSourceBlog;
+	}
+
+	public function
+	SetShowSourceBlog(bool $Val):
+	static {
+	/*//
+	@date 2021-04-09
+	//*/
+
+		$this->ShowSourceBlog = $Val;
+		return $this;
+	}
+
 }
