3f5a3acfe0235d6f67a9fdcefa552f17acb57d08
bobmagicii <bob@pegasusgate.net>
2021-04-30 19:04:00 +0000

    prevent me from deleting an account plan currently in use

---
+++ phinx/20210430180233_atlantis_modify_user_account_plan_index.php
@@ -0,0 +1,37 @@
+<?php // vscode-fold=9
+
+use Phinx\Migration\AbstractMigration;
+
+class AtlantisModifyUserAccountPlanIndex
+extends AbstractMigration {
+
+	public function
+	Up() {
+	/*//
+	operations to perform when upgrading the database with this migration.
+	//*/
+
+		$this->Execute("
+			ALTER TABLE `Users`
+			DROP FOREIGN KEY `FkUserAccountID`;
+
+			ALTER TABLE `Users`
+			CHANGE COLUMN `AccountPlanID` `AccountPlanID` BIGINT(20) UNSIGNED NOT NULL DEFAULT '1' AFTER `ImageIconID`,
+			ADD CONSTRAINT `FkUserAccountPlanID` FOREIGN KEY (`AccountPlanID`) REFERENCES `PegasusDev`.`AccountPlans` (`ID`) ON UPDATE CASCADE ON DELETE RESTRICT;
+		");
+
+		return;
+	}
+
+	public function
+	Down() {
+	/*//
+	operations to perform when downgrading the database with this migration.
+	//*/
+
+		// ...
+
+		return;
+	}
+
+}
